f5c74721dd43b3f36e8b3a6997280ed4f08b7b67
Michael Vu <vu.michaeltien@gmail.com>
2024-01-15 17:48:59 -0500

    repositioned modal toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
+import { Button } from "flowbite-react";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
-
-
-const Navbar = () => {
+const Navbar = ({ setOpenModal }) => {
   const { logOutUser, getToken } = useContext(AuthContext);
 
   return (
@@ -23,36 +22,29 @@ const Navbar = () => {
             to="/explore"
             className="flex flex-col items-center justify-center"
           >
-            <img
-              className="w-6 h-6 flex"
-              src="../src/assets/explore-icon.png"
-            />
+            <img className="w-6 h-6 " src="../src/assets/explore-icon.png" />
             Explore
           </Link>
-          <Link
-            to="/"
-            className="flex flex-col items-center justify-center"
-            style={{ textDecoration: "none" }}
-          >
-            <img className="w-6 h-6 flex" src="../src/assets/create-icon.png" />
-            Create Post
-          </Link>
+          <div className="flex flex-col items-center">
+            <img className="w-6 h-6" src="../src/assets/create-icon.png" />
+            <Button class="outline-none" onClick={() => setOpenModal(true)}>
+              Create Post
+            </Button>
+          </div>
+
           <Link
             to="/profile"
             className="flex flex-col items-center justify-center"
             style={{ textDecoration: "none" }}
           >
-            <img
-              className="w-6 h-6 flex"
-              src="../src/assets/profile-icon.png"
-            />
+            <img className="w-6 h-6" src="../src/assets/profile-icon.png" />
             Profile
           </Link>
           <button
             className="flex flex-col items-center justify-center"
             onClick={logOutUser}
           >
-            <img className="w-6 h-6 flex" src="../src/assets/logout-icon.png" />
+            <img className="w-6 h-6" src="../src/assets/logout-icon.png" />
             Logout
           </button>
         </div>
